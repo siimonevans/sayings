6fffeff3901f9c5600a052d9c0c48c97daf8f7fe
Simon Evans <simon.evans@torchbox.com>
2019-08-02 09:22:12 +0100

    Add saying

--- main.js
+++ main.js
@@ -95,7 +95,8 @@ const data = {
         91: "<div>Chris</div> gitlab search is tripe",
         92: "<div>Ben T</div> Slow, melodic, little bit sludgy but not scuzzy",
         93: "<div>Nick L</div> We’ll definitely have something",
-        94: "<div>Chris</div> overflow hidden ftw"
+        94: "<div>Chris</div> overflow hidden ftw",
+        95: "<div>Todd</div> It’s just ended up as a hairy elephant"
     }
 }
 
